d095598d0ae66175694b68eb5ff8a8a3300de7ad
Jonathan Karlsson <er.jonathan.karlsson@hotmail.com>
2018-11-14 18:41:59 +0100

    Simplified dithering calculations.

--- tiny_structs.cpp
+++ tiny_structs.cpp
@@ -23,57 +23,49 @@ tiny3d::URect tiny3d::Clip(tiny3d::URect a, tiny3d::URect b)
 }
 
 const tiny3d::SInt COLOR_STEP = (TINY3D_BYTE_MAX + 1) / TINY3D_SHADES_PER_COLOR_CHANNEL; // the granularity between one step of 8 bit and one step of 5 bit in terms of 8 bit
-const tiny3d::SInt COLOR_MAX  = COLOR_STEP - 1; // relies on COLOR_STEP being a multiple of 2 (which it should always be)
 
-tiny3d::Color Dither(tiny3d::Color c, tiny3d::UPoint p, const tiny3d::Real *D, tiny3d::UInt N)
+tiny3d::Color Dither(tiny3d::Color c, tiny3d::UPoint p, const tiny3d::SInt *D, tiny3d::UInt N)
 {
-	// 8   -> 5   bits
-	// 256 -> 32 shades
-
-	// this is the threshhold the color needs to clear to shift one hue
-	const SInt threshold = SInt(COLOR_STEP * D[(p.y % N) * N + (p.x % N)]);
-	// the color ratios, i.e. how close one color is to shifting to another
-	const SInt r_ratio = SInt(c.r & COLOR_MAX);
-	const SInt g_ratio = SInt(c.g & COLOR_MAX);
-	const SInt b_ratio = SInt(c.b & COLOR_MAX);
-	// the final color
+	// the dither value to offset the current color by
+	const SInt color_offset = D[(p.y % N) * N + (p.x % N)];
+	// the final color (NOTE: not truncated here, instead the Texture and Image class converts from 8 bit to 5 bit)
 	const Color out = {
-		Byte(Clamp(0, SInt(c.r) + ((r_ratio > threshold) ? COLOR_STEP : 0), 255)),
-		Byte(Clamp(0, SInt(c.g) + ((g_ratio > threshold) ? COLOR_STEP : 0), 255)),
-		Byte(Clamp(0, SInt(c.b) + ((b_ratio > threshold) ? COLOR_STEP : 0), 255)),
+		Byte(Clamp(0, SInt(c.r) + color_offset, 255)),
+		Byte(Clamp(0, SInt(c.g) + color_offset, 255)),
+		Byte(Clamp(0, SInt(c.b) + color_offset, 255)),
 		c.blend
 	};
 	return out;
 }
 
 
-const tiny3d::Real D2x2[4] = {
-	Real(1) / 4, Real(3) / 4,
-	Real(4) / 4, Real(2) / 4
+const tiny3d::SInt D2x2[4] = {
+	SInt(COLOR_STEP * (Real(0) / 4)), SInt(COLOR_STEP * (Real(2) / 4)),
+	SInt(COLOR_STEP * (Real(3) / 4)), SInt(COLOR_STEP * (Real(1) / 4))
 };
 
-const tiny3d::Real D3x3[9] = {
-	Real(1) / 9, Real(8) / 9, Real(4) / 9,
-	Real(7) / 9, Real(6) / 9, Real(3) / 9,
-	Real(5) / 9, Real(2) / 9, Real(9) / 9
+const tiny3d::SInt D3x3[9] = {
+	SInt(COLOR_STEP * (Real(0) / 9)), SInt(COLOR_STEP * (Real(7) / 9)), SInt(COLOR_STEP * (Real(3) / 9)),
+	SInt(COLOR_STEP * (Real(6) / 9)), SInt(COLOR_STEP * (Real(5) / 9)), SInt(COLOR_STEP * (Real(2) / 9)),
+	SInt(COLOR_STEP * (Real(4) / 9)), SInt(COLOR_STEP * (Real(1) / 9)), SInt(COLOR_STEP * (Real(8) / 9))
 };
 
-const tiny3d::Real D4x4[16] = {
-	Real(1)  / 16, Real(9)  / 16, Real(3)  / 16, Real(11) / 16,
-	Real(13) / 16, Real(5)  / 16, Real(15) / 16, Real(7)  / 16,
-	Real(4)  / 16, Real(12) / 16, Real(2)  / 16, Real(10) / 16,
-	Real(16) / 16, Real(8)  / 16, Real(14) / 16, Real(6)  / 16
+const tiny3d::SInt D4x4[16] = {
+	SInt(COLOR_STEP * (Real(0)  / 16)), SInt(COLOR_STEP * (Real(8)  / 16)), SInt(COLOR_STEP * (Real(2)  / 16)), SInt(COLOR_STEP * (Real(10) / 16)),
+	SInt(COLOR_STEP * (Real(12) / 16)), SInt(COLOR_STEP * (Real(4)  / 16)), SInt(COLOR_STEP * (Real(14) / 16)), SInt(COLOR_STEP * (Real(6)  / 16)),
+	SInt(COLOR_STEP * (Real(3)  / 16)), SInt(COLOR_STEP * (Real(11) / 16)), SInt(COLOR_STEP * (Real(1)  / 16)), SInt(COLOR_STEP * (Real(9)  / 16)),
+	SInt(COLOR_STEP * (Real(15) / 16)), SInt(COLOR_STEP * (Real(7)  / 16)), SInt(COLOR_STEP * (Real(13) / 16)), SInt(COLOR_STEP * (Real(5)  / 16))
 };
 
-const tiny3d::Real D8x8[64] = {
-	Real(1)  / 64, Real(49) / 64, Real(13) / 64, Real(61) / 64, Real(4)  / 64, Real(52) / 64, Real(16) / 64, Real(64) / 64,
-	Real(33) / 64, Real(17) / 64, Real(45) / 64, Real(29) / 64, Real(36) / 64, Real(20) / 64, Real(48) / 64, Real(32) / 64,
-	Real(9)  / 64, Real(57) / 64, Real(5)  / 64, Real(53) / 64, Real(12) / 64, Real(60) / 64, Real(8)  / 64, Real(56) / 64,
-	Real(41) / 64, Real(25) / 64, Real(37) / 64, Real(21) / 64, Real(44) / 64, Real(28) / 64, Real(40) / 64, Real(24) / 64,
-	Real(3)  / 64, Real(51) / 64, Real(15) / 64, Real(63) / 64, Real(2)  / 64, Real(50) / 64, Real(14) / 64, Real(62) / 64,
-	Real(35) / 64, Real(19) / 64, Real(47) / 64, Real(31) / 64, Real(34) / 64, Real(18) / 64, Real(46) / 64, Real(30) / 64,
-	Real(11) / 64, Real(59) / 64, Real(7)  / 64, Real(55) / 64, Real(10) / 64, Real(58) / 64, Real(6)  / 64, Real(54) / 64,
-	Real(43) / 64, Real(27) / 64, Real(39) / 64, Real(23) / 64, Real(42) / 64, Real(26) / 64, Real(38) / 64, Real(22) / 64
+const tiny3d::SInt D8x8[64] = {
+	SInt(COLOR_STEP * (Real(0)  / 64)), SInt(COLOR_STEP * (Real(48) / 64)), SInt(COLOR_STEP * (Real(12) / 64)), SInt(COLOR_STEP * (Real(60) / 64)), SInt(COLOR_STEP * (Real(3)  / 64)), SInt(COLOR_STEP * (Real(51) / 64)), SInt(COLOR_STEP * (Real(15) / 64)), SInt(COLOR_STEP * (Real(63) / 64)),
+	SInt(COLOR_STEP * (Real(32) / 64)), SInt(COLOR_STEP * (Real(16) / 64)), SInt(COLOR_STEP * (Real(44) / 64)), SInt(COLOR_STEP * (Real(28) / 64)), SInt(COLOR_STEP * (Real(35) / 64)), SInt(COLOR_STEP * (Real(19) / 64)), SInt(COLOR_STEP * (Real(47) / 64)), SInt(COLOR_STEP * (Real(31) / 64)),
+	SInt(COLOR_STEP * (Real(8)  / 64)), SInt(COLOR_STEP * (Real(56) / 64)), SInt(COLOR_STEP * (Real(4)  / 64)), SInt(COLOR_STEP * (Real(52) / 64)), SInt(COLOR_STEP * (Real(11) / 64)), SInt(COLOR_STEP * (Real(59) / 64)), SInt(COLOR_STEP * (Real(7)  / 64)), SInt(COLOR_STEP * (Real(55) / 64)),
+	SInt(COLOR_STEP * (Real(40) / 64)), SInt(COLOR_STEP * (Real(24) / 64)), SInt(COLOR_STEP * (Real(36) / 64)), SInt(COLOR_STEP * (Real(20) / 64)), SInt(COLOR_STEP * (Real(43) / 64)), SInt(COLOR_STEP * (Real(27) / 64)), SInt(COLOR_STEP * (Real(39) / 64)), SInt(COLOR_STEP * (Real(23) / 64)),
+	SInt(COLOR_STEP * (Real(2)  / 64)), SInt(COLOR_STEP * (Real(50) / 64)), SInt(COLOR_STEP * (Real(14) / 64)), SInt(COLOR_STEP * (Real(62) / 64)), SInt(COLOR_STEP * (Real(1)  / 64)), SInt(COLOR_STEP * (Real(49) / 64)), SInt(COLOR_STEP * (Real(13) / 64)), SInt(COLOR_STEP * (Real(61) / 64)),
+	SInt(COLOR_STEP * (Real(34) / 64)), SInt(COLOR_STEP * (Real(18) / 64)), SInt(COLOR_STEP * (Real(46) / 64)), SInt(COLOR_STEP * (Real(30) / 64)), SInt(COLOR_STEP * (Real(33) / 64)), SInt(COLOR_STEP * (Real(17) / 64)), SInt(COLOR_STEP * (Real(45) / 64)), SInt(COLOR_STEP * (Real(29) / 64)),
+	SInt(COLOR_STEP * (Real(10) / 64)), SInt(COLOR_STEP * (Real(58) / 64)), SInt(COLOR_STEP * (Real(6)  / 64)), SInt(COLOR_STEP * (Real(54) / 64)), SInt(COLOR_STEP * (Real(9)  / 64)), SInt(COLOR_STEP * (Real(57) / 64)), SInt(COLOR_STEP * (Real(5)  / 64)), SInt(COLOR_STEP * (Real(53) / 64)),
+	SInt(COLOR_STEP * (Real(42) / 64)), SInt(COLOR_STEP * (Real(26) / 64)), SInt(COLOR_STEP * (Real(38) / 64)), SInt(COLOR_STEP * (Real(22) / 64)), SInt(COLOR_STEP * (Real(41) / 64)), SInt(COLOR_STEP * (Real(25) / 64)), SInt(COLOR_STEP * (Real(37) / 64)), SInt(COLOR_STEP * (Real(21) / 64))
 };
 
 tiny3d::Color tiny3d::operator+(tiny3d::Color l, tiny3d::Color r)
